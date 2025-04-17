e7ce4484fb3eaeb975db91b74a187923bf39c878
Paul Gricourt <paul.gricourt1@sfr.fr>
2025-04-17 12:07:10 +0200

    Ajout d'un dossier source

---
+++ src/Cryptomonnaie.java
@@ -0,0 +1,12 @@
+public class Cryptomonnaie 
+{
+    private String nom;
+    private double valeurDeJeton; //en €
+    private int    caca;
+
+    public Cryptomonnaie ( String nom, double valeurDeJeton )
+    {
+        this.nom = nom;
+        this.valeurDeJeton = valeurDeJeton;
+    }
+}

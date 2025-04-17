4caa6e9aa1ebffcd41fd3d07a972c83c22945ac3
Paul Gricourt <paul.gricourt1@sfr.fr>
2025-04-17 12:46:01 +0200

    Deuxième version du fichier Cryptomonnaie.java

--- src/Cryptomonnaie.java
+++ src/Cryptomonnaie.java
@@ -1,12 +1,22 @@
-public class Cryptomonnaie 
-{
+public class Cryptomonnaie{
     private String nom;
-    private double valeurDeJeton; //en €
-    private int    caca;
+    private double valeurDeJeton; // Imaginons en euros
 
-    public Cryptomonnaie ( String nom, double valeurDeJeton )
-    {
+    public Cryptomonnaie(String nom, double valeurDeJeton){
         this.nom = nom;
         this.valeurDeJeton = valeurDeJeton;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double getValeurDeJeton() {
+        return valeurDeJeton;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%5s",nom) + ":" + String.format("%10.1f",valeurDeJeton);
     }
 }
